28cff36b77c4a84b464661bad36d925b23db8030
Chris Garson <chris.garson@exlsolutions.net>
2025-10-10 19:27:13 -0400

    Update dataviews.h

--- opensfm/src/map/dataviews.h
+++ opensfm/src/map/dataviews.h
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <geometry/camera.h>
 #include <geometry/similarity.h>
 #include <map/landmark.h>
@@ -9,85 +10,108 @@
 #include <unordered_map>
 
 namespace map {
+
 class Map;
+
+// ------------------------------------------------------
+// ShotView
+// ------------------------------------------------------
 class ShotView {
  public:
-  ShotView(Map& map);
+  explicit ShotView(Map& map);
   Shot& GetShot(const map::ShotId& shot_id);
   bool HasShot(const map::ShotId& shot_id) const;
-  const std::unordered_map<ShotId, Shot>& GetShots() const;
+  const Map::ShotMap& GetShots() const;
   size_t NumberOfShots() const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// PanoShotView
+// ------------------------------------------------------
 class PanoShotView {
  public:
-  PanoShotView(Map& map);
+  explicit PanoShotView(Map& map);
   Shot& GetShot(const map::ShotId& shot_id);
   bool HasShot(const map::ShotId& shot_id) const;
-  const std::unordered_map<ShotId, Shot>& GetShots() const;
+  const Map::ShotMap& GetShots() const;
   size_t NumberOfShots() const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// LandmarkView
+// ------------------------------------------------------
 class LandmarkView {
  public:
-  LandmarkView(Map& map);
+  explicit LandmarkView(Map& map);
   Landmark& GetLandmark(const LandmarkId& lm_id);
   bool HasLandmark(const LandmarkId& lm_id) const;
-  const std::unordered_map<LandmarkId, Landmark>& GetLandmarks() const;
+  const Map::LandmarkMap& GetLandmarks() const;
   size_t NumberOfLandmarks() const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// CameraView
+// ------------------------------------------------------
 class CameraView {
  public:
-  CameraView(Map& map);
+  explicit CameraView(Map& map);
   size_t NumberOfCameras() const;
   geometry::Camera& GetCamera(const CameraId& cam_id);
-  const std::unordered_map<CameraId, geometry::Camera>& GetCameras() const;
+  const Map::CameraMap& GetCameras() const;
   bool HasCamera(const CameraId& cam_id) const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// RigCameraView
+// ------------------------------------------------------
 class RigCameraView {
  public:
   explicit RigCameraView(Map& map);
   size_t NumberOfRigCameras() const;
   RigCamera& GetRigCamera(const RigCameraId& rig_camera_id);
-  const std::unordered_map<RigCameraId, RigCamera>& GetRigCameras() const;
+  const Map::RigCameraMap& GetRigCameras() const;
   bool HasRigCamera(const RigCameraId& rig_camera_id) const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// RigInstanceView
+// ------------------------------------------------------
 class RigInstanceView {
  public:
   explicit RigInstanceView(Map& map);
   size_t NumberOfRigInstances() const;
   RigInstance& GetRigInstance(const RigInstanceId& instance_id);
-  const std::unordered_map<RigInstanceId, RigInstance>& GetRigInstances() const;
+  const Map::RigInstanceMap& GetRigInstances() const;
   bool HasRigInstance(const RigInstanceId& instance_id) const;
 
  private:
   Map& map_;
 };
 
+// ------------------------------------------------------
+// BiasView
+// ------------------------------------------------------
 class BiasView {
  public:
   explicit BiasView(Map& map);
   size_t NumberOfBiases() const;
   geometry::Similarity& GetBias(const CameraId& cam_id);
-  const std::unordered_map<CameraId, geometry::Similarity>& GetBiases() const;
+  const Map::BiasMap& GetBiases() const;
   bool HasBias(const CameraId& cam_id) const;
 
  private:
