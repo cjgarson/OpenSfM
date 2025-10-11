53c0c4e6c27825475f64f965790a7f1f06aeac9f
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 13:11:13 -0400

    Update retriangulation.cc

--- opensfm/src/sfm/src/retriangulation.cc
+++ opensfm/src/sfm/src/retriangulation.cc
@@ -9,8 +9,8 @@
 namespace sfm {
 namespace retriangulation {
 
-void RealignMaps(const sfmmap::map::Map& map_from,
-                 sfmmap::map::Map& map_to,
+void RealignMaps(const sfmmap::Map::Map& map_from,
+                 sfmmap::Map::Map& map_to,
                  bool update_points) {
   const auto& map_from_shots = map_from.GetShots();
 
@@ -19,7 +19,7 @@ void RealignMaps(const sfmmap::map::Map& map_from,
   const auto  from_to_offset = to_ref.ToTopocentric(from_ref.GetLlaRef());
 
   // Transform from each shot in map_to to its corresponding shot in map_from
-  std::unordered_map<sfmmap::map::ShotId, ::geometry::Similarity> from_to_transforms;
+  std::unordered_map<sfmmap::Map::ShotId, ::geometry::Similarity> from_to_transforms;
   for (const auto& [shot_id, shot_to] : map_to.GetShots()) {
     if (!map_from.HasShot(shot_id)) continue;
 
@@ -46,7 +46,7 @@ void RealignMaps(const sfmmap::map::Map& map_from,
     constexpr auto max_dbl = std::numeric_limits<double>::max();
     for (auto& [lm_id, landmark] : map_to.GetLandmarks()) {
       const auto point = landmark.GetGlobalPos();
-      std::pair<double, sfmmap::map::ShotId> best_shot = {max_dbl, ""};
+      std::pair<double, sfmmap::Map::ShotId> best_shot = {max_dbl, ""};
 
       for (const auto& [shot, _obs] : landmark.GetObservations()) {
         if (map_from_shots.find(shot->GetId()) == map_from_shots.end()) continue;
@@ -66,7 +66,7 @@ void RealignMaps(const sfmmap::map::Map& map_from,
   }
 
   // Sync cameras and shots
-  std::unordered_set<sfmmap::map::ShotId> to_delete;
+  std::unordered_set<sfmmap::Map::ShotId> to_delete;
   for (auto& [shot_id, shot_to] : map_to.GetShots()) {
     if (!map_from.HasShot(shot_id)) {
       to_delete.insert(shot_id);
