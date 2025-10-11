cae0b68a5dd41a50239a24734c3c6d5e3081fa36
Chris Garson <chris.garson@exlsolutions.net>
2025-10-10 20:04:31 -0400

    Update map_types.h

--- opensfm/src/map/map_types.h
+++ opensfm/src/map/map_types.h
@@ -5,10 +5,10 @@
 
 namespace map {
 template <typename K, typename V>
-using AlignedUnorderedMap =
-    std::unordered_map<K, V,
-                       std::hash<K>, std::equal_to<K>,
-                       Eigen::aligned_allocator<std::pair<const K, V>>>;
+using AlignedUnorderedMap = std::unordered_map<
+    K, V,
+    std::hash<K>, std::equal_to<K>,
+    Eigen::aligned_allocator<std::pair<const K, V>>>;
 
 // Forward declarations
 struct RigCamera;
@@ -20,12 +20,10 @@ class Camera;
 class Similarity;
 }
 
-// Concrete aliases
 using CameraMap       = AlignedUnorderedMap<std::string, geometry::Camera>;
 using BiasMap         = AlignedUnorderedMap<std::string, geometry::Similarity>;
 using ShotMap         = AlignedUnorderedMap<std::string, Shot>;
 using LandmarkMap     = AlignedUnorderedMap<std::string, Landmark>;
 using RigCameraMap    = AlignedUnorderedMap<std::string, RigCamera>;
 using RigInstanceMap  = AlignedUnorderedMap<std::string, RigInstance>;
-
 }  // namespace map
