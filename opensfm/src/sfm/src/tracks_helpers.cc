0b1a34b7c03459aa25b87030df0ae22baa307a85
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 12:53:24 -0400

    Update tracks_helpers.cc

--- opensfm/src/sfm/src/tracks_helpers.cc
+++ opensfm/src/sfm/src/tracks_helpers.cc
@@ -4,22 +4,20 @@
 #include <unordered_map>
 #include <unordered_set>
 
-using namespace sfmmap;
-
 namespace sfm {
 namespace tracks_helpers {
 
-std::unordered_map<map::ShotId, int> CountTracksPerShot(
-    const map::TracksManager& manager,
-    const std::vector<map::ShotId>& shots,
-    const std::vector<map::TrackId>& tracks) {
+std::unordered_map<sfmmap::map::ShotId, int> CountTracksPerShot(
+    const sfmmap::map::TracksManager& manager,
+    const std::vector<sfmmap::map::ShotId>& shots,
+    const std::vector<sfmmap::map::TrackId>& tracks) {
 
-  std::unordered_set<map::TrackId> tracks_set;
+  std::unordered_set<sfmmap::map::TrackId> tracks_set;
   for (const auto& track : tracks) {
     tracks_set.insert(track);
   }
 
-  std::unordered_map<map::ShotId, int> counts;
+  std::unordered_map<sfmmap::map::ShotId, int> counts;
   for (const auto& shot : shots) {
     const auto& observations = manager.GetShotObservations(shot);
     int sum = 0;
@@ -35,18 +33,18 @@ std::unordered_map<map::ShotId, int> CountTracksPerShot(
   return counts;
 }
 
-void AddConnections(map::TracksManager& manager,
-                    const map::ShotId& shot_id,
-                    const std::vector<map::TrackId>& connections) {
-  map::Observation observation;
+void AddConnections(sfmmap::map::TracksManager& manager,
+                    const sfmmap::map::ShotId& shot_id,
+                    const std::vector<sfmmap::map::TrackId>& connections) {
+  sfmmap::map::Observation observation;
   for (const auto& connection : connections) {
     manager.AddObservation(shot_id, connection, observation);
   }
 }
 
-void RemoveConnections(map::TracksManager& manager,
-                       const map::ShotId& shot_id,
-                       const std::vector<map::TrackId>& connections) {
+void RemoveConnections(sfmmap::map::TracksManager& manager,
+                       const sfmmap::map::ShotId& shot_id,
+                       const std::vector<sfmmap::map::TrackId>& connections) {
   for (const auto& connection : connections) {
     manager.RemoveObservation(shot_id, connection);
   }
