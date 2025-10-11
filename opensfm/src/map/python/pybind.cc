c0d857403228070cca52189ec81b593deaed10ea
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 14:11:19 -0400

    Update pybind.cc

--- opensfm/src/map/python/pybind.cc
+++ opensfm/src/map/python/pybind.cc
@@ -18,43 +18,45 @@
 
 #include <iostream>
 #include <typeinfo>
+
 namespace py = pybind11;
 
+// Generic OptionalValue<T> wrapper
 template <typename T>
 void DeclareShotMeasurement(py::module &m, const std::string &type_name) {
   using SM = foundation::OptionalValue<T>;
-  std::string class_name = std::string("ShotMeasurement") + type_name;
+  const std::string class_name = "ShotMeasurement" + type_name;
   py::class_<SM>(m, class_name.c_str())
       .def(py::init<>())
       .def_property_readonly("has_value", &SM::HasValue)
-      .def_property("value", py::overload_cast<>(&SM::Value, py::const_),
-                    py::overload_cast<>(&SM::Value))
+      .def_property("value",
+                    [](const SM& s) { return s.Value(); },
+                    [](SM& s, const T& v) { s.SetValue(v); })
       .def("reset", &SM::Reset);
 }
 
-// Module definition
 PYBIND11_MODULE(pymap, m) {
   py::module::import("opensfm.pygeometry");
   py::module::import("opensfm.pygeo");
 
-  // Forward declarations for classes
+  // Forward declarations to break cycles
   py::class_<sfmmap::Shot> shotCls(m, "Shot");
   py::class_<sfmmap::Map>  mapCls(m, "Map");
 
-  // Declare ShotMeasurement types for Int, Double, etc.
+  // Optional<T> bindings used by ShotMeasurements
   DeclareShotMeasurement<int>(m, "Int");
   DeclareShotMeasurement<double>(m, "Double");
   DeclareShotMeasurement<Vec3d>(m, "Vec3d");
   DeclareShotMeasurement<std::string>(m, "String");
 
-  // Enum for error types (Pixel, Normalized, Angular)
+  // ErrorType enum
   py::enum_<sfmmap::Map::ErrorType>(m, "ErrorType")
       .value("Pixel",      sfmmap::Map::Pixel)
       .value("Normalized", sfmmap::Map::Normalized)
       .value("Angular",    sfmmap::Map::Angular)
       .export_values();
 
-  // Observation struct binding
+  // Observation
   py::class_<sfmmap::Observation>(m, "Observation")
       .def(py::init<double,double,double,int,int,int,int,int,int>(),
            py::arg("x"), py::arg("y"), py::arg("s"),
@@ -70,11 +72,9 @@ PYBIND11_MODULE(pymap, m) {
       .def_readwrite("instance",     &sfmmap::Observation::instance_id)
       .def_readonly_static("NO_SEMANTIC_VALUE",
                            &sfmmap::Observation::NO_SEMANTIC_VALUE)
-      .def("copy", [](const sfmmap::Observation &obs) {
-           return sfmmap::Observation(obs);
-       });
+      .def("copy", [](const sfmmap::Observation &o) { return sfmmap::Observation(o); });
 
-  // Landmark class binding
+  // Landmark
   py::class_<sfmmap::Landmark>(m, "Landmark")
       .def(py::init<const sfmmap::LandmarkId&, const Vec3d&>())
       .def_readonly("id", &sfmmap::Landmark::id_)
@@ -92,7 +92,7 @@ PYBIND11_MODULE(pymap, m) {
                     &sfmmap::Landmark::GetColor,
                     &sfmmap::Landmark::SetColor);
 
-  // ShotMeasurements struct binding
+  // ShotMeasurements
   py::class_<sfmmap::ShotMeasurements>(m, "ShotMeasurements")
       .def(py::init<>())
       .def_readwrite("gps_accuracy",     &sfmmap::ShotMeasurements::gps_accuracy_)
@@ -110,69 +110,90 @@ PYBIND11_MODULE(pymap, m) {
                     &sfmmap::ShotMeasurements::SetAttributes)
       .def("set", &sfmmap::ShotMeasurements::Set);
 
-  // RigCamera struct binding
+  // RigCamera (simple struct)
   py::class_<sfmmap::RigCamera>(m, "RigCamera")
-      .def(py::init([](const geometry::Pose &pose,
-                       sfmmap::RigCameraId id) {
-            sfmmap::RigCamera rc;
-            rc.pose = pose;
-            rc.id   = std::move(id);
-            return rc;
-         }))
+      .def(py::init([](const geometry::Pose& pose, sfmmap::RigCameraId id) {
+        sfmmap::RigCamera rc; rc.pose = pose; rc.id = std::move(id); return rc;
+      }))
       .def_readwrite("id",   &sfmmap::RigCamera::id)
       .def_readwrite("pose", &sfmmap::RigCamera::pose);
 
-  // RigInstance class binding
+  // RigInstance
   py::class_<sfmmap::RigInstance>(m, "RigInstance")
       .def(py::init<sfmmap::RigInstanceId>())
       .def_property_readonly("id", &sfmmap::RigInstance::GetId)
       .def_property("pose",
-                    py::overload_cast<>(&sfmmap::RigInstance::GetPose, py::const_),
-                    &sfmmap::RigInstance::SetPose,
-                    py::return_value_policy::reference_internal)
+        [](const sfmmap::RigInstance& ri) -> const geometry::Pose& { return ri.GetPose(); },
+        [](sfmmap::RigInstance& ri, const geometry::Pose& p) { ri.SetPose(p); },
+        py::return_value_policy::reference_internal)
       .def_property_readonly("shots",
-                    py::overload_cast<>(&sfmmap::RigInstance::GetShots, py::const_),
-                    py::return_value_policy::reference_internal)
+        [](const sfmmap::RigInstance& ri)
+          -> const std::unordered_map<sfmmap::ShotId, sfmmap::Shot*>& {
+          return ri.GetShots();
+        }, py::return_value_policy::reference_internal)
       .def_property_readonly("rig_cameras",
-                    py::overload_cast<>(&sfmmap::RigInstance::GetRigCameras, py::const_),
-                    py::return_value_policy::reference_internal)
-      .def("add_shot", &sfmmap::RigInstance::AddShot)
+        [](const sfmmap::RigInstance& ri)
+          -> const std::unordered_map<sfmmap::ShotId, sfmmap::RigCamera*>& {
+          return ri.GetRigCameras();
+        }, py::return_value_policy::reference_internal)
+      .def("add_shot",    &sfmmap::RigInstance::AddShot)
       .def("remove_shot", &sfmmap::RigInstance::RemoveShot);
 
-  // Shot class binding
+  // Shot
   shotCls
       .def(py::init<const sfmmap::ShotId&, const geometry::Camera&, const geometry::Pose&>())
       .def_readonly("id", &sfmmap::Shot::id_)
       .def_property("pose",
-                    py::overload_cast<>(&sfmmap::Shot::GetPose, py::const_),
-                    &sfmmap::Shot::SetPose,
-                    py::return_value_policy::reference_internal)
-      .def_property_readonly("camera", &sfmmap::Shot::GetCamera,
-                             py::return_value_policy::reference_internal)
-      .def("get_landmarks", &sfmmap::Shot::GetLandmarkObservations)
+        [](const sfmmap::Shot& s) -> const geometry::Pose& { return *s.GetPose(); },
+        [](sfmmap::Shot& s, const geometry::Pose& p) { s.SetPose(p); },
+        py::return_value_policy::reference_internal)
+      .def_property_readonly("camera",
+        [](const sfmmap::Shot& s) { return s.GetCamera(); },
+        py::return_value_policy::reference_internal)
+      .def("get_landmarks",
+        [](sfmmap::Shot& s)
+          -> std::map<sfmmap::Landmark*, sfmmap::Observation, sfmmap::KeyCompare,
+                      Eigen::aligned_allocator<std::pair<sfmmap::Landmark* const, sfmmap::Observation>>>& {
+          return s.GetLandmarkObservations();
+        }, py::return_value_policy::reference_internal)
       .def("remove_landmark", &sfmmap::Shot::RemoveLandmarkObservation);
 
-  // Map class binding
+  // Map (expose only the essentials needed by python side here)
   mapCls
       .def(py::init<>())
-      .def("get_shot", py::overload_cast<const sfmmap::ShotId&>(&sfmmap::Map::GetShot, py::const_),
-           py::return_value_policy::reference_internal)
-      .def("get_landmark", py::overload_cast<const sfmmap::LandmarkId&>(&sfmmap::Map::GetLandmark, py::const_),
-           py::return_value_policy::reference_internal)
+      .def("get_shot",
+        [](sfmmap::Map& m, const sfmmap::ShotId& id) -> sfmmap::Shot& { return m.GetShot(id); },
+        py::return_value_policy::reference_internal)
+      .def("get_landmark",
+        [](sfmmap::Map& m, const sfmmap::LandmarkId& id) -> sfmmap::Landmark& { return m.GetLandmark(id); },
+        py::return_value_policy::reference_internal)
       .def("create_camera", &sfmmap::Map::CreateCamera)
-      .def("create_shot", py::overload_cast<const sfmmap::ShotId&, const sfmmap::CameraId&,
-                                            const sfmmap::RigCameraId&, const sfmmap::RigInstanceId&,
-                                            const geometry::Pose&>(&sfmmap::Map::CreateShot))
+      .def("create_shot",
+        (sfmmap::Shot& (sfmmap::Map::*)(const sfmmap::ShotId&, const sfmmap::CameraId&,
+                                        const sfmmap::RigCameraId&, const sfmmap::RigInstanceId&,
+                                        const geometry::Pose&)) &sfmmap::Map::CreateShot,
+        py::return_value_policy::reference_internal)
       .def("remove_shot", &sfmmap::Map::RemoveShot)
-      .def("create_landmark", &sfmmap::Map::CreateLandmark)
-      .def("remove_landmark", py::overload_cast<const sfmmap::LandmarkId&>(&sfmmap::Map::RemoveLandmark))
-      .def("add_observation", py::overload_cast<sfmmap::Shot*, sfmmap::Landmark*, const sfmmap::Observation&>(&sfmmap::Map::AddObservation))
-      .def("remove_observation", &sfmmap::Map::RemoveObservation)
+      .def("create_landmark", &sfmmap::Map::CreateLandmark,
+           py::return_value_policy::reference_internal)
+      .def("remove_landmark",
+           (void (sfmmap::Map::*)(const sfmmap::LandmarkId&)) &sfmmap::Map::RemoveLandmark)
+      .def("add_observation",
+           (void (sfmmap::Map::*)(sfmmap::Shot* const, sfmmap::Landmark* const,
+                                  const sfmmap::Observation&)) &sfmmap::Map::AddObservation)
+      .def("remove_observation",
+           (void (sfmmap::Map::*)(const sfmmap::ShotId&, const sfmmap::LandmarkId&)) &sfmmap::Map::RemoveObservation)
       .def("clear_observations_and_landmarks", &sfmmap::Map::ClearObservationsAndLandmarks)
       .def("get_topocentric_converter", &sfmmap::Map::GetTopocentricConverter,
            py::return_value_policy::reference_internal)
-      .def("get_shots", &sfmmap::Map::GetShots,
-           py::return_value_policy::reference_internal)
-      .def("get_landmarks", &sfmmap::Map::GetLandmarks,
-           py::return_value_policy::reference_internal);
+      .def("get_shots",
+           [](sfmmap::Map& m)
+             -> std::unordered_map<sfmmap::ShotId, sfmmap::Shot&, sfmmap::KeyCompare>& {
+             return m.GetShots();
+           }, py::return_value_policy::reference_internal)
+      .def("get_landmarks",
+           [](sfmmap::Map& m)
+             -> std::unordered_map<sfmmap::LandmarkId, sfmmap::Landmark&, sfmmap::KeyCompare>& {
+             return m.GetLandmarks();
+           }, py::return_value_policy::reference_internal);
 }
