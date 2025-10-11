55be4f702eb6917ae39cfa1fa4110c38f26edcbf
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 13:50:27 -0400

    Update retriangulation.cc

--- opensfm/src/sfm/src/retriangulation.cc
+++ opensfm/src/sfm/src/retriangulation.cc
@@ -9,93 +9,81 @@
 namespace sfm {
 namespace retriangulation {
 
-void RealignMaps(const sfmmap::Map::Map& map_from,
-                 sfmmap::Map::Map& map_to,
+void RealignMaps(const sfmmap::Map& map_from, sfmmap::Map& map_to,
                  bool update_points) {
   const auto& map_from_shots = map_from.GetShots();
 
-  const auto& from_ref = map_from.GetTopocentricConverter();
-  const auto& to_ref   = map_to.GetTopocentricConverter();
+  const auto& from_ref   = map_from.GetTopocentricConverter();
+  const auto& to_ref     = map_to.GetTopocentricConverter();
   const auto  from_to_offset = to_ref.ToTopocentric(from_ref.GetLlaRef());
 
-  // Transform from each shot in map_to to its corresponding shot in map_from
-  std::unordered_map<sfmmap::Map::ShotId, ::geometry::Similarity> from_to_transforms;
-  for (const auto& [shot_id, shot_to] : map_to.GetShots()) {
-    if (!map_from.HasShot(shot_id)) continue;
+  // first, record transforms that remap points of 'to'
+  std::unordered_map<sfmmap::ShotId, geometry::Similarity> from_to_transforms;
+  for (const auto& shot_to : map_to.GetShots()) {
+    if (!map_from.HasShot(shot_to.first)) {
+      continue;
+    }
+    const auto& shot_from = map_from.GetShot(shot_to.first);
+    auto shot_from_pose   = *shot_from.GetPose();
+    const auto shot_to_pose = *shot_to.second.GetPose();
 
-    const auto& shot_from = map_from.GetShot(shot_id);
-    auto shot_from_pose = *shot_from.GetPose();
-    const auto shot_to_pose = *shot_to.GetPose();
-
+    // put 'from' in LLA of 'to'
     shot_from_pose.SetOrigin(shot_from_pose.GetOrigin() + from_to_offset);
 
-    const double scale = (shot_from.scale != 0.0) ? (1.0 / shot_from.scale) : 1.0;
-    const Mat3d R_to_from =
-        shot_from_pose.RotationCameraToWorld() *
-        shot_to_pose.RotationWorldToCamera();
-    const Vec3d t_from_to =
-        -scale * R_to_from * shot_to_pose.GetOrigin() +
-        shot_from_pose.GetOrigin();
-
-    from_to_transforms[shot_id] =
-        ::geometry::Similarity(R_to_from, t_from_to, scale);
+    // compute similarity that brings 'to' shot to 'from' shot
+    geometry::Similarity sim = geometry::Similarity::FromRigToRig(shot_to_pose, shot_from_pose);
+    from_to_transforms[shot_to.first] = sim;
   }
 
-  // Optionally realign all landmarks
-  if (update_points) {
-    constexpr auto max_dbl = std::numeric_limits<double>::max();
-    for (auto& [lm_id, landmark] : map_to.GetLandmarks()) {
-      const auto point = landmark.GetGlobalPos();
-      std::pair<double, sfmmap::Map::ShotId> best_shot = {max_dbl, ""};
-
-      for (const auto& [shot, _obs] : landmark.GetObservations()) {
-        if (map_from_shots.find(shot->GetId()) == map_from_shots.end()) continue;
-        const Vec3d ray = point - shot->GetPose()->GetOrigin();
-        const double dist2 = ray.squaredNorm();
-        if (dist2 < best_shot.first)
-          best_shot = {dist2, shot->GetId()};
-      }
-
-      if (best_shot.first == max_dbl) continue;
-      const auto& ref_shot = best_shot.second;
-      const auto it = from_to_transforms.find(ref_shot);
-      if (it == from_to_transforms.end()) continue;
-
-      landmark.SetGlobalPos(it->second.Transform(landmark.GetGlobalPos()));
-    }
-  }
-
-  // Sync cameras and shots
-  std::unordered_set<sfmmap::Map::ShotId> to_delete;
+  // apply transforms
+  std::unordered_set<sfmmap::ShotId> to_delete;
   for (auto& [shot_id, shot_to] : map_to.GetShots()) {
     if (!map_from.HasShot(shot_id)) {
       to_delete.insert(shot_id);
       continue;
     }
-
-    const auto& shot_from = map_from.GetShot(shot_id);
-    auto& camera_to = map_to.GetCamera(shot_to.GetCamera()->id);
-
-    camera_to.SetParametersValues(
-        shot_from.GetCamera()->GetParametersValues());
-    shot_to.scale    = shot_from.scale;
-    shot_to.merge_cc = shot_from.merge_cc;
+    const auto& sim = from_to_transforms[shot_id];
+    // update shot pose
+    shot_to.SetPose(sim.TransformPose(*shot_to.GetPose()));
+    // update each landmark observed in this shot
+    for (auto& lm_obs : shot_to.GetLandmarkObservations()) {
+      const sfmmap::LandmarkId& lm_id = lm_obs.first->id_;
+      if (!map_from.HasLandmark(lm_id)) {
+        continue;
+      }
+      auto& lm_to = map_to.GetLandmark(lm_id);
+      const auto& lm_from = map_from.GetLandmark(lm_id);
+      // transfer point
+      lm_to.SetGlobalPos(sim.Transform(lm_from.GetGlobalPos()));
+    }
   }
-
-  // Update rig instance poses
-  for (auto& [instance_id, rig_instance_to] : map_to.GetRigInstances()) {
-    for (const auto& [shot_id, _shot_ptr] : rig_instance_to.GetShots()) {
-      if (map_from_shots.find(shot_id) == map_from_shots.end()) continue;
-      const auto& shot_from = map_from_shots.at(shot_id);
-      auto& to_pose = rig_instance_to.GetPose();
-      to_pose = shot_from.GetRigInstance()->GetPose();
-      to_pose.SetOrigin(to_pose.GetOrigin() + from_to_offset);
-      break;
+  // remove shots and orphaned points that couldn't be aligned
+  for (const auto& shot_id : to_delete) {
+    map_to.RemoveShot(shot_id);
+  }
+  // optionally, remove points not seen by any aligned shot
+  if (update_points) {
+    std::unordered_set<sfmmap::LandmarkId> to_remove;
+    for (const auto& lm_pair : map_to.GetLandmarks()) {
+      const auto& lm_id = lm_pair.first;
+      if (!map_from.HasLandmark(lm_id)) {
+        to_remove.insert(lm_id);
+      }
+    }
+    for (const auto& lm_id : to_remove) {
+      map_to.RemoveLandmark(lm_id);
     }
   }
 
-  for (const auto& shot_id : to_delete)
-    map_to.RemoveShot(shot_id);
+  // align biases (if any)
+  for (auto& cam_pair : map_to.GetCameras()) {
+    const auto& cam_id = cam_pair.first;
+    if (map_from.HasBias(cam_id) && map_to.HasBias(cam_id)) {
+      // combine the two bias transformations
+      geometry::Similarity new_bias = map_from.GetBias(cam_id) * map_to.GetBias(cam_id);
+      map_to.SetBias(cam_id, new_bias);
+    }
+  }
 }
 
 }  // namespace retriangulation
