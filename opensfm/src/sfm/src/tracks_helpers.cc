20bd26d917ba90fb67db67f6cf8e18e3c3fda014
Chris Garson <chris.garson@exlsolutions.net>
2025-10-10 22:05:18 -0400

    Update tracks_helpers.cc

--- opensfm/src/sfm/src/tracks_helpers.cc
+++ opensfm/src/sfm/src/tracks_helpers.cc
@@ -1,25 +1,29 @@
 #include <sfm/tracks_helpers.h>
+#include <map/tracks_manager.h>
 
 #include <unordered_map>
 #include <unordered_set>
 
 namespace sfm {
 namespace tracks_helpers {
+
 std::unordered_map<map::ShotId, int> CountTracksPerShot(
-    const map::TracksManager& manager, const std::vector<map::ShotId>& shots,
+    const map::TracksManager& manager,
+    const std::vector<map::ShotId>& shots,
     const std::vector<map::TrackId>& tracks) {
+
   std::unordered_set<map::TrackId> tracks_set;
   for (const auto& track : tracks) {
     tracks_set.insert(track);
   }
+
   std::unordered_map<map::ShotId, int> counts;
   for (const auto& shot : shots) {
     const auto& observations = manager.GetShotObservations(shot);
-
     int sum = 0;
     for (const auto& obs : observations) {
-      const auto& trackID = obs.first;
-      if (tracks_set.find(trackID) == tracks_set.end()) {
+      const auto& track_id = obs.first;
+      if (tracks_set.find(track_id) == tracks_set.end()) {
         continue;
       }
       ++sum;
@@ -29,7 +33,8 @@ std::unordered_map<map::ShotId, int> CountTracksPerShot(
   return counts;
 }
 
-void AddConnections(map::TracksManager& manager, const map::ShotId& shot_id,
+void AddConnections(map::TracksManager& manager,
+                    const map::ShotId& shot_id,
                     const std::vector<map::TrackId>& connections) {
   map::Observation observation;
   for (const auto& connection : connections) {
@@ -37,11 +42,13 @@ void AddConnections(map::TracksManager& manager, const map::ShotId& shot_id,
   }
 }
 
-void RemoveConnections(map::TracksManager& manager, const map::ShotId& shot_id,
+void RemoveConnections(map::TracksManager& manager,
+                       const map::ShotId& shot_id,
                        const std::vector<map::TrackId>& connections) {
   for (const auto& connection : connections) {
     manager.RemoveObservation(shot_id, connection);
   }
 }
+
 }  // namespace tracks_helpers
 }  // namespace sfm
