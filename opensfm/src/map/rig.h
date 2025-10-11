83093ff2906e36e7bf5356529dc5769100728d11
Chris Garson <chris.garson@exlsolutions.net>
2025-10-10 21:57:16 -0400

    Update rig.h

--- opensfm/src/map/rig.h
+++ opensfm/src/map/rig.h
@@ -34,10 +34,10 @@ struct RigCamera {
   ::geometry::Pose pose;
 
   /* Unique identifier of this RigCamera */
-  map::RigCameraId id;
+  map::Map::RigCameraId id;
 
   RigCamera() = default;
-  RigCamera(const ::geometry::Pose& p, const map::RigCameraId& rid)
+  RigCamera(const ::geometry::Pose& p, const map::Map::RigCameraId& rid)
       : pose(p), id(rid) {}
 };
 
@@ -45,25 +45,25 @@ class RigInstance {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  map::RigInstanceId id;
+  map::Map::RigInstanceId id;
 
   RigInstance() = default;
   explicit RigInstance(const RigInstanceId instance_id) : id(instance_id) {}
 
   // Getters
-  const std::unordered_map<map::ShotId, map::Shot*>& GetShots() const {
+  const std::unordered_map<map::Map::ShotId, map::Map::Shot*>& GetShots() const {
     return shots_;
   }
-  std::unordered_map<map::ShotId, map::Shot*>& GetShots() { return shots_; }
+  std::unordered_map<map::Map::ShotId, map::Map::Shot*>& GetShots() { return shots_; }
 
-  std::unordered_map<map::ShotId, map::RigCamera*>& GetRigCameras() {
+  std::unordered_map<map::Map::ShotId, map::Map::RigCamera*>& GetRigCameras() {
     return shots_rig_cameras_;
   }
-  const std::unordered_map<map::ShotId, map::RigCamera*>& GetRigCameras() const {
+  const std::unordered_map<map::Map::ShotId, map::Map::RigCamera*>& GetRigCameras() const {
     return shots_rig_cameras_;
   }
 
-  std::set<map::ShotId> GetShotIDs() const;
+  std::set<map::Map::ShotId> GetShotIDs() const;
   size_t NumberOfShots() const;
 
   // Pose
@@ -72,36 +72,36 @@ class RigInstance {
   void SetPose(const ::geometry::Pose& pose) { pose_ = pose; }
 
   // Add a new shot to this instance
-  void AddShot(map::RigCamera* rig_camera, map::Shot* shot);
+  void AddShot(map::Map::RigCamera* rig_camera, map::Map::Shot* shot);
 
   // Update instance pose and shot's poses wrt. to a given shot of the instance
-  void UpdateInstancePoseWithShot(const map::ShotId& shot_id,
+  void UpdateInstancePoseWithShot(const map::Map::ShotId& shot_id,
                                   const ::geometry::Pose& shot_pose);
 
   // Update pose of this instance's RigCamera
-  void UpdateRigCameraPose(const map::RigCameraId& rig_camera_id,
+  void UpdateRigCameraPose(const map::Map::RigCameraId& rig_camera_id,
                            const ::geometry::Pose& pose);
 
   // Removal
-  void RemoveShot(const map::ShotId& shot_id);
+  void RemoveShot(const map::Map::ShotId& shot_id);
 
  private:
   // Actual instantiation of a rig : each shot gets mapped to some RigCamera
-  std::unordered_map<map::ShotId, map::Shot*> shots_;
-  std::unordered_map<map::ShotId, map::RigCamera*> shots_rig_cameras_;
+  std::unordered_map<map::Map::ShotId, map::Map::Shot*> shots_;
+  std::unordered_map<map::Map::ShotId, map::Map::RigCamera*> shots_rig_cameras_;
 
   // Pose of this rig coordinate frame wrt. world coordinates
   ::geometry::Pose pose_;
 
   // For VARIABLE rigs, each instance has its own copy of RigCameras
   std::unordered_map<
-      map::ShotId,
-      foundation::OptionalValue<map::RigCamera>,
-      std::hash<map::ShotId>,
-      std::equal_to<map::ShotId>,
+      map::Map::ShotId,
+      foundation::OptionalValue<map::Map::RigCamera>,
+      std::hash<map::Map::ShotId>,
+      std::equal_to<map::Map::ShotId>,
       Eigen::aligned_allocator<
-          std::pair<const map::ShotId,
-                    foundation::OptionalValue<map::RigCamera>>>>
+          std::pair<const map::Map::ShotId,
+                    foundation::OptionalValue<map::Map::RigCamera>>>>
       own_rig_cameras_;
 };
 
