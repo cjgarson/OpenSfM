c330e30c7055bf29d14c7d8d446a06a97ae2ae9b
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 12:17:53 -0400

    Update map.h

--- opensfm/src/map/map.h
+++ opensfm/src/map/map.h
@@ -20,8 +20,7 @@
 
 namespace sfmmap {
 
-// Forward-declare the view classes to avoid a circular include.
-// (map.h does NOT include dataviews.h; dataviews.h will include map.h)
+// Forward declare view classes to avoid circular includes
 class CameraView;
 class ShotView;
 class PanoShotView;
@@ -34,92 +33,80 @@ class Map {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  // ---- Aliases for aligned containers (note leading ::geometry) ----
-  using CameraMap = std::unordered_map<
-      CameraId, ::geometry::Camera, std::hash<CameraId>, std::equal_to<CameraId>,
+  using CameraMap = std::unordered_map<CameraId, ::geometry::Camera,
+      std::hash<CameraId>, std::equal_to<CameraId>,
       Eigen::aligned_allocator<std::pair<const CameraId, ::geometry::Camera>>>;
 
-  using BiasMap = std::unordered_map<
-      CameraId, ::geometry::Similarity, std::hash<CameraId>, std::equal_to<CameraId>,
+  using BiasMap = std::unordered_map<CameraId, ::geometry::Similarity,
+      std::hash<CameraId>, std::equal_to<CameraId>,
       Eigen::aligned_allocator<std::pair<const CameraId, ::geometry::Similarity>>>;
 
-  using ShotMap = std::unordered_map<
-      ShotId, Shot, std::hash<ShotId>, std::equal_to<ShotId>,
+  using ShotMap = std::unordered_map<ShotId, Shot,
+      std::hash<ShotId>, std::equal_to<ShotId>,
       Eigen::aligned_allocator<std::pair<const ShotId, Shot>>>;
 
-  using LandmarkMap = std::unordered_map<
-      LandmarkId, Landmark, std::hash<LandmarkId>, std::equal_to<LandmarkId>,
+  using LandmarkMap = std::unordered_map<LandmarkId, Landmark,
+      std::hash<LandmarkId>, std::equal_to<LandmarkId>,
       Eigen::aligned_allocator<std::pair<const LandmarkId, Landmark>>>;
 
-  using RigCameraMap = std::unordered_map<
-      RigCameraId, RigCamera, std::hash<RigCameraId>, std::equal_to<RigCameraId>,
+  using RigCameraMap = std::unordered_map<RigCameraId, RigCamera,
+      std::hash<RigCameraId>, std::equal_to<RigCameraId>,
       Eigen::aligned_allocator<std::pair<const RigCameraId, RigCamera>>>;
 
-  using RigInstanceMap = std::unordered_map<
-      RigInstanceId, RigInstance, std::hash<RigInstanceId>, std::equal_to<RigInstanceId>,
+  using RigInstanceMap = std::unordered_map<RigInstanceId, RigInstance,
+      std::hash<RigInstanceId>, std::equal_to<RigInstanceId>,
       Eigen::aligned_allocator<std::pair<const RigInstanceId, RigInstance>>>;
 
-  // --- Deep Copy ---
-  static std::unique_ptr<Map> DeepCopy(const Map& map,
-                                       bool copy_observations = false);
+  // Deep copy entire map
+  static std::unique_ptr<Map> DeepCopy(const Map& map, bool copy_observations = false);
 
-  // --- Camera Methods ---
+  // Camera methods
   ::geometry::Camera& GetCamera(const CameraId& cam_id);
   const ::geometry::Camera& GetCamera(const CameraId& cam_id) const;
   ::geometry::Camera& CreateCamera(const ::geometry::Camera& cam);
-  CameraView GetCameraView();  // defined in dataviews.cc
+  CameraView GetCameraView();
   bool HasCamera(const CameraId& cam_id) const { return cameras_.count(cam_id) > 0; }
   const CameraMap& GetCameras() const { return cameras_; }
   CameraMap& GetCameras() { return cameras_; }
 
-  // --- Shot Methods ---
+  // Shot methods
   Shot& CreateShot(const ShotId& shot_id, const CameraId& camera_id,
-                   const RigCameraId& rig_camera_id,
-                   const RigInstanceId& instance_id,
+                   const RigCameraId& rig_camera_id, const RigInstanceId& instance_id,
                    const ::geometry::Pose& pose);
   Shot& CreateShot(const ShotId& shot_id, const CameraId& camera_id,
-                   const RigCameraId& rig_camera_id,
-                   const RigInstanceId& instance_id);
-
+                   const RigCameraId& rig_camera_id, const RigInstanceId& instance_id);
   const Shot& GetShot(const ShotId& shot_id) const;
   Shot& GetShot(const ShotId& shot_id);
   bool HasShot(const ShotId& shot_id) const { return shots_.count(shot_id) > 0; }
   const ShotMap& GetShots() const { return shots_; }
   ShotMap& GetShots() { return shots_; }
-
   Shot& UpdateShot(const Shot& other_shot);
   void RemoveShot(const ShotId& shot_id);
-  ShotView GetShotView();  // defined in dataviews.cc
+  ShotView GetShotView();
 
-  // --- PanoShots ---
+  // Panoramic shot methods
   Shot& CreatePanoShot(const ShotId& shot_id, const CameraId& camera_id,
-                       const RigCameraId& rig_camera_id,
-                       const RigInstanceId& instance_id,
+                       const RigCameraId& rig_camera_id, const RigInstanceId& instance_id,
                        const ::geometry::Pose& pose);
-
   Shot& GetPanoShot(const ShotId& shot_id);
   const Shot& GetPanoShot(const ShotId& shot_id) const;
   bool HasPanoShot(const ShotId& shot_id) const { return pano_shots_.count(shot_id) > 0; }
   const ShotMap& GetPanoShots() const { return pano_shots_; }
   ShotMap& GetPanoShots() { return pano_shots_; }
-
   void RemovePanoShot(const ShotId& shot_id);
   Shot& UpdatePanoShot(const Shot& other_shot);
-  PanoShotView GetPanoShotView();  // defined in dataviews.cc
+  PanoShotView GetPanoShotView();
 
-  // --- Rigs ---
-  RigCamera& CreateRigCamera(const map::RigCamera& rig_camera);
-  RigInstance& CreateRigInstance(const map::RigInstanceId& instance_id);
-
-  void RemoveRigInstance(const map::RigInstanceId& instance_id);
+  // Rig (multi-camera) methods
+  RigCamera& CreateRigCamera(const RigCamera& rig_camera);
+  RigInstance& CreateRigInstance(const RigInstanceId& instance_id);
+  void RemoveRigInstance(const RigInstanceId& instance_id);
   RigInstance& UpdateRigInstance(const RigInstance& other_rig_instance);
-
   size_t NumberOfRigCameras() const;
   RigCamera& GetRigCamera(const RigCameraId& rig_camera_id);
   const RigCameraMap& GetRigCameras() const { return rig_cameras_; }
   RigCameraMap& GetRigCameras() { return rig_cameras_; }
   bool HasRigCamera(const RigCameraId& rig_camera_id) const;
-
   size_t NumberOfRigInstances() const;
   RigInstance& GetRigInstance(const RigInstanceId& instance_id);
   const RigInstance& GetRigInstance(const RigInstanceId& instance_id) const;
@@ -127,52 +114,51 @@ class Map {
   RigInstanceMap& GetRigInstances() { return rig_instances_; }
   bool HasRigInstance(const RigInstanceId& instance_id) const;
 
-  // --- Landmarks ---
+  // Landmark methods
   Landmark& CreateLandmark(const LandmarkId& lm_id, const Vec3d& global_pos);
-
   const Landmark& GetLandmark(const LandmarkId& lm_id) const;
   Landmark& GetLandmark(const LandmarkId& lm_id);
   const LandmarkMap& GetLandmarks() const { return landmarks_; }
   LandmarkMap& GetLandmarks() { return landmarks_; }
   bool HasLandmark(const LandmarkId& lm_id) const { return landmarks_.count(lm_id) > 0; }
-  LandmarkView GetLandmarkView();  // defined in dataviews.cc
-
+  LandmarkView GetLandmarkView();
   void RemoveLandmark(const Landmark* const lm);
   void RemoveLandmark(const LandmarkId& lm_id);
 
-  // --- Observations ---
+  // Observations management
   void AddObservation(Shot* const shot, Landmark* const lm, const Observation& obs);
   void AddObservation(const ShotId& shot_id, const LandmarkId& lm_id, const Observation& obs);
   void RemoveObservation(const ShotId& shot_id, const LandmarkId& lm_id);
   void ClearObservationsAndLandmarks();
-  void CleanLandmarksBelowMinObservations(const size_t min_observations);
+  void CleanLandmarksBelowMinObservations(size_t min_observations);
 
-  // --- Map information ---
+  // Map statistics
   size_t NumberOfShots() const { return shots_.size(); }
   size_t NumberOfPanoShots() const { return pano_shots_.size(); }
   size_t NumberOfLandmarks() const { return landmarks_.size(); }
   size_t NumberOfCameras() const { return cameras_.size(); }
   size_t NumberOfBiases() const { return bias_.size(); }
 
-  // --- Bias ---
-  BiasView GetBiasView();  // defined in dataviews.cc
+  // Camera bias (for geospatial alignment)
+  BiasView GetBiasView();
   ::geometry::Similarity& GetBias(const CameraId& camera_id);
   void SetBias(const CameraId& camera_id, const ::geometry::Similarity& transform);
   bool HasBias(const CameraId& cam_id) const { return bias_.count(cam_id) > 0; }
   const BiasMap& GetBiases() const { return bias_; }
   BiasMap& GetBiases() { return bias_; }
 
-  // --- TopocentricConverter ---
+  // Topocentric reference for coordinates
   const geo::TopocentricConverter& GetTopocentricConverter() const { return topo_conv_; }
-  void SetTopocentricConverter(const double lat, const double longitude, const double alt) {
+  void SetTopocentricConverter(double lat, double longitude, double alt) {
     topo_conv_.lat_ = lat;
     topo_conv_.long_ = longitude;
     topo_conv_.alt_ = alt;
   }
 
+  // Convert to TracksManager (for bundler/tracker use)
   TracksManager ToTracksManager() const;
 
-  // --- Reprojection ---
+  // Reprojection error computation
   enum ErrorType { Pixel = 0x0, Normalized = 0x1, Angular = 0x2 };
   std::unordered_map<ShotId, std::unordered_map<LandmarkId, Vec2d>>
   ComputeReprojectionErrors(const TracksManager& tracks_manager, const ErrorType& error_type) const;
@@ -189,8 +175,7 @@ class Map {
   LandmarkMap landmarks_;
   RigInstanceMap rig_instances_;
   RigCameraMap rig_cameras_;
-
   geo::TopocentricConverter topo_conv_;
 };
 
-}  // namespace map
+}  // namespace sfmmap
