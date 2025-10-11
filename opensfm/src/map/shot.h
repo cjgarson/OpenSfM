fd2d9f5b845ef14176e47580fe689bc64b00b47f
Chris Garson <chris.garson@exlsolutions.net>
2025-10-10 20:05:11 -0400

    Update shot.h

--- opensfm/src/map/shot.h
+++ opensfm/src/map/shot.h
@@ -12,6 +12,7 @@
 #include <unordered_map>
 
 namespace map {
+
 class Map;
 
 struct ShotMesh {
@@ -36,14 +37,13 @@ struct ShotMeasurements {
   foundation::OptionalValue<Vec3d> opk_angles_;
   foundation::OptionalValue<int> orientation_;
   foundation::OptionalValue<std::string> sequence_key_;
-  void Set(const ShotMeasurements& other);
+  void Set(const ShotMeasurements& other) { *this = other; }
 
-  // Store any additional attributes
   std::map<std::string, std::string> attributes_;
   const auto& GetAttributes() const { return attributes_; }
   auto& GetMutableAttributes() { return attributes_; }
-  void SetAttributes(const std::map<std::string, std::string>& attributes) {
-    attributes_ = attributes;
+  void SetAttributes(const std::map<std::string, std::string>& a) {
+    attributes_ = a;
   }
 };
 
@@ -51,17 +51,16 @@ class Shot {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  // Shot construction
-  Shot(const ShotId& shot_id, const geometry::Camera* const shot_camera,
+  Shot(const ShotId& shot_id, const ::geometry::Camera* const shot_camera,
        RigInstance* rig_instance, RigCamera* rig_camera,
-       const geometry::Pose& pose);
-  Shot(const ShotId& shot_id, const geometry::Camera* const shot_camera,
+       const ::geometry::Pose& pose);
+  Shot(const ShotId& shot_id, const ::geometry::Camera* const shot_camera,
        RigInstance* rig_instance, RigCamera* rig_camera);
-  Shot(const ShotId& shot_id, const geometry::Camera& shot_camera,
-       const geometry::Pose& pose);
+  Shot(const ShotId& shot_id, const ::geometry::Camera& shot_camera,
+       const ::geometry::Pose& pose);
+
   ShotId GetId() const { return id_; }
 
-  // Rig
   bool IsInRig() const;
   void SetRig(RigInstance* rig_instance, RigCamera* rig_camera);
   RigInstance* GetRigInstance() const { return rig_instance_; }
@@ -69,17 +68,14 @@ class Shot {
   const RigInstanceId& GetRigInstanceId() const;
   const RigCameraId& GetRigCameraId() const;
 
-  // Pose
-  void SetPose(const geometry::Pose& pose);
-  const geometry::Pose* const GetPose() const;
-  geometry::Pose* const GetPose();
+  void SetPose(const ::geometry::Pose& pose);
+  const ::geometry::Pose* const GetPose() const;
+  ::geometry::Pose* const GetPose();
   Mat4d GetWorldToCam() const { return GetPose()->WorldToCamera(); }
   Mat4d GetCamToWorld() const { return GetPose()->CameraToWorld(); }
 
-  // Landmark management
-  const std::map<
-      Landmark*, Observation, KeyCompare,
-      Eigen::aligned_allocator<std::pair<Landmark* const, Observation>>>&
+  const std::map<Landmark*, Observation, KeyCompare,
+                 Eigen::aligned_allocator<std::pair<Landmark* const, Observation>>>&
   GetLandmarkObservations() const {
     return landmark_observations_;
   }
@@ -88,96 +84,75 @@ class Shot {
   GetLandmarkObservations() {
     return landmark_observations_;
   }
+
   std::vector<Landmark*> ComputeValidLandmarks() {
-    std::vector<Landmark*> valid_landmarks;
-    valid_landmarks.reserve(landmark_observations_.size());
-    for (const auto& lm_obs : landmark_observations_) {
-      valid_landmarks.push_back(lm_obs.first);
-    }
-    return valid_landmarks;
+    std::vector<Landmark*> v;
+    v.reserve(landmark_observations_.size());
+    for (auto& lm_obs : landmark_observations_) v.push_back(lm_obs.first);
+    return v;
   }
 
-  // Observation management
   const Observation& GetObservation(const FeatureId id) const {
     return landmark_observations_.at(landmark_id_.at(id));
   }
   void CreateObservation(Landmark* lm, const Observation& obs) {
-    landmark_observations_.insert(std::make_pair(lm, obs));
-    landmark_id_.insert(std::make_pair(obs.feature_id, lm));
+    landmark_observations_.insert({lm, obs});
+    landmark_id_.insert({obs.feature_id, lm});
   }
   Observation* GetLandmarkObservation(Landmark* lm) {
     return &landmark_observations_.at(lm);
   }
   Landmark* GetObservationLandmark(const FeatureId id) {
-    auto find_landmark = landmark_id_.find(id);
-    if (find_landmark == landmark_id_.end()) {
-      return nullptr;
-    } else {
-      return find_landmark->second;
-    }
+    auto it = landmark_id_.find(id);
+    return it == landmark_id_.end() ? nullptr : it->second;
   }
   void RemoveLandmarkObservation(const FeatureId id);
 
-  // Metadata such as GPS, IMU, time
-  const ShotMeasurements& GetShotMeasurements() const {
-    return shot_measurements_;
-  }
+  const ShotMeasurements& GetShotMeasurements() const { return shot_measurements_; }
   ShotMeasurements& GetShotMeasurements() { return shot_measurements_; }
   void SetShotMeasurements(const ShotMeasurements& other) {
     shot_measurements_.Set(other);
   }
 
-  // Comparisons
-  bool operator==(const Shot& shot) const { return id_ == shot.id_; }
-  bool operator!=(const Shot& shot) const { return !(*this == shot); }
-  bool operator<(const Shot& shot) const { return id_ < shot.id_; }
-  bool operator<=(const Shot& shot) const { return id_ <= shot.id_; }
-  bool operator>(const Shot& shot) const { return id_ > shot.id_; }
-  bool operator>=(const Shot& shot) const { return id_ >= shot.id_; }
-  const geometry::Camera* const GetCamera() const { return shot_camera_; }
+  bool operator==(const Shot& s) const { return id_ == s.id_; }
+  bool operator!=(const Shot& s) const { return !(*this == s); }
 
-  // Camera-related
+  const ::geometry::Camera* const GetCamera() const { return shot_camera_; }
+
   Vec2d Project(const Vec3d& global_pos) const;
   MatX2d ProjectMany(const MatX3d& points) const;
   Vec3d Bearing(const Vec2d& point) const;
   MatX3d BearingMany(const MatX2d& points) const;
 
-  // Covariance
   MatXd GetCovariance() const { return covariance_.Value(); }
   void SetCovariance(const MatXd& cov) { covariance_.SetValue(cov); }
 
  public:
-  const ShotId id_;  // the file name
-
-  // Ad-hoc merge-specific data
+  const ShotId id_;
   ShotMesh mesh;
   long int merge_cc{0};
   double scale{1.0};
 
  private:
-  geometry::Pose GetPoseInRig() const;
+  ::geometry::Pose GetPoseInRig() const;
 
-  // Pose
-  mutable std::unique_ptr<geometry::Pose> pose_;
+  mutable std::unique_ptr<::geometry::Pose> pose_;
   foundation::OptionalValue<MatXd> covariance_;
 
-  // Rig data (can optionally belong to the shot)
   foundation::OptionalValue<RigInstance> own_rig_instance_;
   foundation::OptionalValue<RigCamera> own_rig_camera_;
-  RigInstance* rig_instance_;
-  RigCamera* rig_camera_;
+  RigInstance* rig_instance_{nullptr};
+  RigCamera* rig_camera_{nullptr};
 
-  // Camera pointer (can optionally belong to the shot)
-  foundation::OptionalValue<geometry::Camera> own_camera_;
-  const geometry::Camera* const shot_camera_;
+  foundation::OptionalValue<::geometry::Camera> own_camera_;
+  const ::geometry::Camera* const shot_camera_;
 
-  // Metadata
   ShotMeasurements shot_measurements_;
 
-  // In OpenSfM, we use a map to reproduce a similar behaviour
   std::map<Landmark*, Observation, KeyCompare,
            Eigen::aligned_allocator<std::pair<Landmark* const, Observation>>>
       landmark_observations_;
   std::unordered_map<FeatureId, Landmark*> landmark_id_;
 };
+
 }  // namespace map
