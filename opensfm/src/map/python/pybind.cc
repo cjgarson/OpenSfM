404eb88b0ea6e171ef53d57f951474a611fee96f
Chris Garson <chris.garson@exlsolutions.net>
2025-10-11 13:11:38 -0400

    Update pybind.cc

--- opensfm/src/map/python/pybind.cc
+++ opensfm/src/map/python/pybind.cc
@@ -53,105 +53,105 @@ PYBIND11_MODULE(pymap, m) {
     py::module::import("opensfm.pygeo");
 
     // Forward declarations
-    py::class_<sfmmap::map::Shot> shotCls(m, "Shot");
-    py::class_<sfmmap::map::Map>  mapCls(m, "Map");
+    py::class_<sfmmap::Map::Shot> shotCls(m, "Shot");
+    py::class_<sfmmap::Map::Map>  mapCls(m, "Map");
 
     DeclareShotMeasurement<int>(m, "Int");
     DeclareShotMeasurement<double>(m, "Double");
     DeclareShotMeasurement<Vec3d>(m, "Vec3d");
     DeclareShotMeasurement<std::string>(m, "String");
 
-    py::enum_<sfmmap::map::Map::ErrorType>(m, "ErrorType")
-        .value("Pixel", sfmmap::map::Map::Pixel)
-        .value("Normalized", sfmmap::map::Map::Normalized)
-        .value("Angular", sfmmap::map::Map::Angular)
+    py::enum_<sfmmap::Map::Map::ErrorType>(m, "ErrorType")
+        .value("Pixel", sfmmap::Map::Map::Pixel)
+        .value("Normalized", sfmmap::Map::Map::Normalized)
+        .value("Angular", sfmmap::Map::Map::Angular)
         .export_values();
 
-    py::class_<sfmmap::map::Observation>(m, "Observation")
+    py::class_<sfmmap::Map::Observation>(m, "Observation")
         .def(py::init<double,double,double,int,int,int,int,int,int>(),
              py::arg("x"), py::arg("y"), py::arg("s"),
              py::arg("r"), py::arg("g"), py::arg("b"),
              py::arg("feature"),
-             py::arg("segmentation") = sfmmap::map::Observation::NO_SEMANTIC_VALUE,
-             py::arg("instance")     = sfmmap::map::Observation::NO_SEMANTIC_VALUE)
-        .def_readwrite("point",        &sfmmap::map::Observation::point)
-        .def_readwrite("scale",        &sfmmap::map::Observation::scale)
-        .def_readwrite("id",           &sfmmap::map::Observation::feature_id)
-        .def_readwrite("color",        &sfmmap::map::Observation::color)
-        .def_readwrite("segmentation", &sfmmap::map::Observation::segmentation_id)
-        .def_readwrite("instance",     &sfmmap::map::Observation::instance_id)
+             py::arg("segmentation") = sfmmap::Map::Observation::NO_SEMANTIC_VALUE,
+             py::arg("instance")     = sfmmap::Map::Observation::NO_SEMANTIC_VALUE)
+        .def_readwrite("point",        &sfmmap::Map::Observation::point)
+        .def_readwrite("scale",        &sfmmap::Map::Observation::scale)
+        .def_readwrite("id",           &sfmmap::Map::Observation::feature_id)
+        .def_readwrite("color",        &sfmmap::Map::Observation::color)
+        .def_readwrite("segmentation", &sfmmap::Map::Observation::segmentation_id)
+        .def_readwrite("instance",     &sfmmap::Map::Observation::instance_id)
         .def_readonly_static("NO_SEMANTIC_VALUE",
-                             &sfmmap::map::Observation::NO_SEMANTIC_VALUE)
-        .def("copy", [](const sfmmap::map::Observation &to_copy) {
-            return sfmmap::map::Observation(to_copy);
+                             &sfmmap::Map::Observation::NO_SEMANTIC_VALUE)
+        .def("copy", [](const sfmmap::Map::Observation &to_copy) {
+            return sfmmap::Map::Observation(to_copy);
         });
 
-    py::class_<sfmmap::map::Landmark>(m, "Landmark")
-        .def(py::init<const sfmmap::map::LandmarkId&, const Vec3d&>())
-        .def_readonly("id", &sfmmap::map::Landmark::id_)
+    py::class_<sfmmap::Map::Landmark>(m, "Landmark")
+        .def(py::init<const sfmmap::Map::LandmarkId&, const Vec3d&>())
+        .def_readonly("id", &sfmmap::Map::Landmark::id_)
         .def_property("coordinates",
-                      &sfmmap::map::Landmark::GetGlobalPos,
-                      &sfmmap::map::Landmark::SetGlobalPos)
-        .def("get_observations", &sfmmap::map::Landmark::GetObservations,
+                      &sfmmap::Map::Landmark::GetGlobalPos,
+                      &sfmmap::Map::Landmark::SetGlobalPos)
+        .def("get_observations", &sfmmap::Map::Landmark::GetObservations,
              py::return_value_policy::reference_internal)
         .def("number_of_observations",
-             &sfmmap::map::Landmark::NumberOfObservations)
+             &sfmmap::Map::Landmark::NumberOfObservations)
         .def_property("reprojection_errors",
-                      &sfmmap::map::Landmark::GetReprojectionErrors,
-                      &sfmmap::map::Landmark::SetReprojectionErrors)
+                      &sfmmap::Map::Landmark::GetReprojectionErrors,
+                      &sfmmap::Map::Landmark::SetReprojectionErrors)
         .def_property("color",
-                      &sfmmap::map::Landmark::GetColor,
-                      &sfmmap::map::Landmark::SetColor);
+                      &sfmmap::Map::Landmark::GetColor,
+                      &sfmmap::Map::Landmark::SetColor);
 
-    py::class_<sfmmap::map::ShotMeasurements>(m, "ShotMeasurements")
+    py::class_<sfmmap::Map::ShotMeasurements>(m, "ShotMeasurements")
         .def(py::init<>())
-        .def_readwrite("gps_accuracy",     &sfmmap::map::ShotMeasurements::gps_accuracy_)
-        .def_readwrite("gps_position",     &sfmmap::map::ShotMeasurements::gps_position_)
-        .def_readwrite("orientation",      &sfmmap::map::ShotMeasurements::orientation_)
-        .def_readwrite("capture_time",     &sfmmap::map::ShotMeasurements::capture_time_)
-        .def_readwrite("gravity_down",     &sfmmap::map::ShotMeasurements::gravity_down_)
-        .def_readwrite("compass_angle",    &sfmmap::map::ShotMeasurements::compass_angle_)
-        .def_readwrite("compass_accuracy", &sfmmap::map::ShotMeasurements::compass_accuracy_)
-        .def_readwrite("opk_angles",       &sfmmap::map::ShotMeasurements::opk_angles_)
-        .def_readwrite("opk_accuracy",     &sfmmap::map::ShotMeasurements::opk_accuracy_)
-        .def_readwrite("sequence_key",     &sfmmap::map::ShotMeasurements::sequence_key_)
+        .def_readwrite("gps_accuracy",     &sfmmap::Map::ShotMeasurements::gps_accuracy_)
+        .def_readwrite("gps_position",     &sfmmap::Map::ShotMeasurements::gps_position_)
+        .def_readwrite("orientation",      &sfmmap::Map::ShotMeasurements::orientation_)
+        .def_readwrite("capture_time",     &sfmmap::Map::ShotMeasurements::capture_time_)
+        .def_readwrite("gravity_down",     &sfmmap::Map::ShotMeasurements::gravity_down_)
+        .def_readwrite("compass_angle",    &sfmmap::Map::ShotMeasurements::compass_angle_)
+        .def_readwrite("compass_accuracy", &sfmmap::Map::ShotMeasurements::compass_accuracy_)
+        .def_readwrite("opk_angles",       &sfmmap::Map::ShotMeasurements::opk_angles_)
+        .def_readwrite("opk_accuracy",     &sfmmap::Map::ShotMeasurements::opk_accuracy_)
+        .def_readwrite("sequence_key",     &sfmmap::Map::ShotMeasurements::sequence_key_)
         .def_property("attributes",
-                      &sfmmap::map::ShotMeasurements::GetAttributes,
-                      &sfmmap::map::ShotMeasurements::SetAttributes)
-        .def("set", &sfmmap::map::ShotMeasurements::Set);
+                      &sfmmap::Map::ShotMeasurements::GetAttributes,
+                      &sfmmap::Map::ShotMeasurements::SetAttributes)
+        .def("set", &sfmmap::Map::ShotMeasurements::Set);
 
-    py::class_<sfmmap::map::RigCamera>(m, "RigCamera")
+    py::class_<sfmmap::Map::RigCamera>(m, "RigCamera")
         .def(py::init([](const ::geometry::Pose &pose,
-                         sfmmap::map::RigCameraId id) {
-            sfmmap::map::RigCamera rc;
+                         sfmmap::Map::RigCameraId id) {
+            sfmmap::Map::RigCamera rc;
             rc.pose = pose;
             rc.id   = std::move(id);
             return rc;
         }))
-        .def_readwrite("id",   &sfmmap::map::RigCamera::id)
-        .def_readwrite("pose", &sfmmap::map::RigCamera::pose);
+        .def_readwrite("id",   &sfmmap::Map::RigCamera::id)
+        .def_readwrite("pose", &sfmmap::Map::RigCamera::pose);
 
-    py::class_<sfmmap::map::RigInstance>(m, "RigInstance")
-        .def(py::init<sfmmap::map::RigInstanceId>())
-        .def_property_readonly("id", &sfmmap::map::RigInstance::GetId)
+    py::class_<sfmmap::Map::RigInstance>(m, "RigInstance")
+        .def(py::init<sfmmap::Map::RigInstanceId>())
+        .def_property_readonly("id", &sfmmap::Map::RigInstance::GetId)
         .def_property("pose",
-                      py::overload_cast<>(&sfmmap::map::RigInstance::GetPose),
-                      &sfmmap::map::RigInstance::SetPose,
+                      py::overload_cast<>(&sfmmap::Map::RigInstance::GetPose),
+                      &sfmmap::Map::RigInstance::SetPose,
                       py::return_value_policy::reference_internal)
         .def_property_readonly("shots",
                       py::overload_cast<>(
-                          &sfmmap::map::RigInstance::GetShots, py::const_),
+                          &sfmmap::Map::RigInstance::GetShots, py::const_),
                       py::return_value_policy::reference_internal)
-        .def("add_shot", &sfmmap::map::RigInstance::AddShot)
-        .def("remove_shot", &sfmmap::map::RigInstance::RemoveShot);
+        .def("add_shot", &sfmmap::Map::RigInstance::AddShot)
+        .def("remove_shot", &sfmmap::Map::RigInstance::RemoveShot);
 
     shotCls
-        .def(py::init<const sfmmap::map::ShotId&, const ::geometry::Camera&, const ::geometry::Pose&>())
-        .def_readonly("id", &sfmmap::map::Shot::id_)
-        .def_property("pose", py::overload_cast<>(&sfmmap::map::Shot::GetPose),
-                      &sfmmap::map::Shot::SetPose,
+        .def(py::init<const sfmmap::Map::ShotId&, const ::geometry::Camera&, const ::geometry::Pose&>())
+        .def_readonly("id", &sfmmap::Map::Shot::id_)
+        .def_property("pose", py::overload_cast<>(&sfmmap::Map::Shot::GetPose),
+                      &sfmmap::Map::Shot::SetPose,
                       py::return_value_policy::reference_internal)
-        .def_property_readonly("camera", &sfmmap::map::Shot::GetCamera,
+        .def_property_readonly("camera", &sfmmap::Map::Shot::GetCamera,
                                py::return_value_policy::reference_internal);
 
     mapCls.def(py::init<>());
