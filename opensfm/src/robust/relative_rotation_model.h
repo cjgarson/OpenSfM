71724f05359d0d0836a7830442144201634dc37e
Chris Garson <chris.garson@exlsolutions.net>
2025-10-13 00:15:39 -0400

    Update relative_rotation_model.h

--- opensfm/src/robust/relative_rotation_model.h
+++ opensfm/src/robust/relative_rotation_model.h
@@ -6,6 +6,7 @@
 
 class RelativeRotation : public Model<RelativeRotation, 1, 1> {
  public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   using Error = typename Model<RelativeRotation, 1, 1>::Error;
   using Type = Eigen::Matrix3d;
   using Data = std::pair<Eigen::Vector3d, Eigen::Vector3d>;
