5ad26e555bdb0bfc6b270354dc0c539e987164a1
Chris Garson <chris.garson@exlsolutions.net>
2025-10-12 20:09:01 -0400

    Update types.h

--- opensfm/src/foundation/types.h
+++ opensfm/src/foundation/types.h
@@ -2,9 +2,6 @@
 
 #include <Eigen/Eigen>
 
-#include <vector>
-#include <array>
-
 EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Vector3d)
 EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix3d)
 EIGEN_DEFINE_STL_VECTOR_SPECIALIZATION(Eigen::Matrix<double,3,4>)
