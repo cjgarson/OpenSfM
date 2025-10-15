f3e77b831f071682c52b85e2b187cc3bfcda3fe4
Chris Garson <chris.garson@exlsolutions.net>
2025-10-15 03:32:45 -0400

    Update ba_helpers.cc

--- opensfm/src/sfm/src/ba_helpers.cc
+++ opensfm/src/sfm/src/ba_helpers.cc
@@ -776,7 +776,7 @@ py::dict BAHelpers::BundleShotPoses(
 
   ba.SetNumThreads(16); //cg
   ba.SetMaxNumIterations(7);
-  ba.SetLinearSolverType("SPARSE_SCHUR"); //cg
+  ba.SetLinearSolverType("DENSE_QR"); //cg
 
   
   const auto t_setup = std::chrono::high_resolution_clock::now();
@@ -786,7 +786,7 @@ py::dict BAHelpers::BundleShotPoses(
       report["num_shots"] = static_cast<int>(shot_ids.size());
       report["map_shots"] = static_cast<int>(map.GetShots().size());
       return report;
-  }*/
+  } */
   
   { py::gil_scoped_release release; ba.Run(); }
   const auto t_run   = std::chrono::high_resolution_clock::now();
